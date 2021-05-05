7e31a6e4e02c59d2b6d9024d8698cdac13de4b26
PauloQuintino <pauloqfs16@gmail.com>
2021-05-05 13:50:17 -0300

    Static charts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,35 @@
+import BarChart from './components/barchart';
 import DataTable from './components/datatable';
+import DonutChart from './components/donutchart';
 import Footer from './components/footer';
 import NavBar from './components/navbar';
 
 function App() {
   return (
-    <> 
+    <>
       <NavBar />
       <div className="container">
-        <h1 className="text-primary">Quintino - DS Sales</h1>
-      <DataTable/>
+        <h1 className="text-primary py-3">Sales Dashboard</h1>
+
+        <div className="row px-3">
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Success Fee (%)</h5>
+            <BarChart />
+          </div>
+
+          <div className="col-sm-6">
+          <h5 className="text-center text-secondary">All sales</h5>
+            <DonutChart />
+          </div>
+        </div>
+
+        <div className="py-3">
+          <h2 className="text-primary">All Sales</h2>
+        </div>
+
+        <DataTable />
       </div>
-      <Footer/>
+      <Footer />
     </>
   );
 }

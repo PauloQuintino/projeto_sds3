d01705f86910a1bdd266a55e9297884c508019a0
PauloQuintino <pauloqfs16@gmail.com>
2021-05-06 15:15:28 -0300

    Pagination

---
+++ backend/src/main/java/com/paulo/dsvendas/service/SaleService.java
@@ -0,0 +1,31 @@
+package com.paulo.dsvendas.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.paulo.dsvendas.dto.SaleDTO;
+import com.paulo.dsvendas.entities.Sale;
+import com.paulo.dsvendas.repositories.SaleRepository;
+import com.paulo.dsvendas.repositories.SellerRepository;
+
+@Service
+public class SaleService {
+
+	@Autowired
+	private SaleRepository repository;
+	
+	@Autowired
+	private SellerRepository sellerRepository;
+	
+	//busca paginada de Sales
+	@Transactional(readOnly = true)
+	public Page<SaleDTO> findAll(Pageable pageable){
+		sellerRepository.findAll();
+		Page<Sale> result = repository.findAll(pageable);
+		return result.map(x -> new SaleDTO(x));
+	}
+	
+}
